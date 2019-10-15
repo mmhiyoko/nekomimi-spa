baf52f0f8ef026ecf0d65a3edb22ab406c7b825b
mmhiyoko <mmhiyoko@gmail.com>
2019-10-15 19:30:59 +0900

    add search event listener

--- src/js/pages/Top.tsx
+++ src/js/pages/Top.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TopPage = () => {
-  return <h1>Top Page</h1>;
+  const [searchValue, setSearchValue] = useState<string>("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.currentTarget.value);
+  };
+  const handleClick = () => {
+    // TODO: call api
+    console.log(searchValue);
+  };
+  return (
+    <>
+      <h1>Top Page</h1>
+      <input type="text" value={searchValue} onChange={handleChange} />
+      <button type="button" onClick={handleClick}>
+        検索
+      </button>
+    </>
+  );
 };
 
 export default TopPage;
