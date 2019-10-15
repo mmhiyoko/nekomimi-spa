06aa840f4c1cfff5445495d05bcd666eff9373bd
mmhiyoko <mmhiyoko@gmail.com>
2019-10-15 19:08:28 +0900

    create Top component

--- src/js/pages/Root.tsx
+++ src/js/pages/Root.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import TopPage from "./Top";
 
 const RootPage = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={() => <h1>Top Page</h1>} />
+        <Route exact path="/" component={TopPage} />
         <Route path="/watch/:id" component={() => <h1>Watch Page</h1>} />
         <Route component={() => <h1>404</h1>} />
       </Switch>
---
+++ src/js/pages/Top.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TopPage = () => {
+  return <h1>Top Page</h1>;
+};
+
+export default TopPage;
