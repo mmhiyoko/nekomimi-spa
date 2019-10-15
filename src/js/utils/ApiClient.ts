14d0024a71ec111b9409ecbfce59fadddb8cf125
mmhiyoko <mmhiyoko@gmail.com>
2019-10-15 20:25:02 +0900

    call youtube data api v3

--- src/js/utils/ApiClient.ts
+++ src/js/utils/ApiClient.ts
@@ -1,6 +1,16 @@
+import axios from "axios";
+const { YOUTUBE_API_KEY } = process.env;
+const ENDPOINT_URL = "https://www.googleapis.com/youtube/v3";
+
 export default class ApiClint {
-  static search(q: string): Promise<any[]> {
-    // TODO: call api
-    return Promise.resolve([q]);
+  static async search(q: string): Promise<any[]> {
+    const { data } = await axios.get(`${ENDPOINT_URL}/search`, {
+      params: {
+        q,
+        part: "snippet",
+        key: YOUTUBE_API_KEY
+      }
+    });
+    return data.items;
   }
 }
