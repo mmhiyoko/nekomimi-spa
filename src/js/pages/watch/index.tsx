fad9e85c5b2e7c684666baab7ee790682c500a24
mmhiyoko <mmhiyoko@gmail.com>
2019-10-24 20:14:32 +0900

    fetch Video

--- src/js/pages/watch/index.tsx
+++ src/js/pages/watch/index.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import ApiClint, { Video } from "../../utils/ApiClient";
 
-const WatchPage = () => {
-  return <h1>Watch Page</h1>;
+type Params = {
+  id: string;
+};
+
+type Props = RouteComponentProps<Params>;
+
+const WatchPage = (props: Props) => {
+  const { match } = props;
+  const { id: videoId } = match.params;
+  const [video, setVideo] = useState<Video>();
+  const fetchVideo = async () => {
+    try {
+      const [result] = await ApiClint.videos(videoId);
+      setVideo(result);
+    } catch (e) {
+      console.log("TODO");
+    } finally {
+      console.log("TODO");
+    }
+  };
+  useEffect(() => {
+    fetchVideo();
+  }, []);
+  console.log(video);
+  return <h1>{`Watch Page ${videoId}`}</h1>;
 };
 
 export default WatchPage;
--- src/js/utils/ApiClient.ts
+++ src/js/utils/ApiClient.ts
@@ -4,6 +4,7 @@ const { YOUTUBE_API_KEY } = process.env;
 const ENDPOINT_URL = "https://www.googleapis.com/youtube/v3";
 
 export type SearchResult = gapi.client.youtube.SearchResult;
+export type Video = gapi.client.youtube.Video;
 
 export default class ApiClint {
   static async search(q: string): Promise<SearchResult[]> {
@@ -14,6 +15,20 @@ export default class ApiClint {
           q,
           part: "snippet",
           type: "video",
+          key: YOUTUBE_API_KEY
+        }
+      }
+    );
+    return data.items!;
+  }
+
+  static async videos(id: string): Promise<Video[]> {
+    const { data } = await axios.get<gapi.client.youtube.VideoListResponse>(
+      `${ENDPOINT_URL}/videos`,
+      {
+        params: {
+          id,
+          part: "snippet,player",
           key: YOUTUBE_API_KEY
         }
       }
