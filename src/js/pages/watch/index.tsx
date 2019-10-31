3e02a65000435d721d870a49648617832f69f81c
mmhiyoko <mmhiyoko@gmail.com>
2019-10-31 19:25:37 +0900

    VideoPlayer component

--- src/js/pages/watch/index.tsx
+++ src/js/pages/watch/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import ApiClint, { Video } from "../../utils/ApiClient";
+import VideoPlayer from "./organisms/VideoPlayer";
 
 type Params = {
   id: string;
@@ -11,21 +12,44 @@ type Props = RouteComponentProps<Params>;
 const WatchPage = (props: Props) => {
   const { match } = props;
   const { id: videoId } = match.params;
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
   const [video, setVideo] = useState<Video>();
   const fetchVideo = async () => {
     try {
+      setIsError(false);
+      setIsFetching(true);
       const [result] = await ApiClint.videos(videoId);
       setVideo(result);
     } catch (e) {
-      console.log("TODO");
+      setIsError(true);
     } finally {
-      console.log("TODO");
+      setIsFetching(false);
     }
   };
   useEffect(() => {
     fetchVideo();
   }, []);
-  return <h1>{`Watch Page ${videoId}`}</h1>;
+  if (isFetching || video == null) {
+    return <h1>Loading...</h1>;
+  }
+  if (isError) {
+    return (
+      <div>
+        Error Occured
+        <button type="button" onClick={fetchVideo}>
+          再試行
+        </button>
+      </div>
+    );
+  }
+  console.log(video);
+  return (
+    <>
+      <h1>{`WatchPage: ${videoId}`}</h1>
+      <VideoPlayer player={video.player!} />
+    </>
+  );
 };
 
 export default WatchPage;
